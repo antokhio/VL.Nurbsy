127aebc55092fdcb722f7f0275fa33085fb7ece7
antokhio <antokhio@gmail.com>
2026-01-23 19:21:51 +0300

    fix: bezier curve getpoint, added Nurbsy.Rendering

---
+++ Nurbsy.Rendering/BezierSurfaceModel.cs
@@ -0,0 +1,13 @@
+﻿using Stride.Graphics;
+using Stride.Rendering.ProceduralModels;
+
+namespace VL.Nurbsy.Stride
+{
+    public class BezierSurfaceModel : PrimitiveProceduralModelBase
+    {
+        protected override GeometricMeshData<VertexPositionNormalTexture> CreatePrimitiveMeshData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
